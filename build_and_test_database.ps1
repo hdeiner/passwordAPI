61ad0125ba931aad52fa874ca423dfe02911cc4d
hdeiner <howard.deiner@deinersoft.com>
2018-05-30 15:10:39 -0400

    Add support for automatically building and testing a SQL Server database running inside a Windows Docker container.

--- build_and_test_database.ps1
+++ build_and_test_database.ps1
@@ -1,21 +1,23 @@
+Write-host Kill the current Docker sqlsvrtest container
 .\docker_reset_sqlsvrtest_container.ps1
+Write-host Crete a fresh Docker sqlsvrtest container
 .\docker_start_sqlsvrtest_container.ps1
 
-#Write-host "Pause 30 seconds to allow SQL Server to start up"
-#sleep 30
+Write-host "Pause 60 seconds to allow SQL Server to start up"
+Start-Sleep -s 60
 
-#echo Install tSQLt
-#mkdir -p test_results
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -Q "ALTER DATABASE master SET TRUSTWORTHY ON;"
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -i "tSQLt/tSQLt.class.sql" -o "test_results/Sales_Database_tSQLt_Install.txt"
-#
-#echo Create database
-#liquibase --changeLogFile=src/test/db/changelog-with-unit-tests.xml update
-#
-#echo Run the tSQLt unit tests
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -Q "sp_configure @configname=clr_enabled, @configvalue=1"
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -Q "RECONFIGURE"
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -Q "EXEC tSQLt.RunAll"
-#
-#echo Make sure that the unit tests ran
-#sqlcmd -S localhost -U SA -P 'Strong!Passw0rd' -Q "EXEC tSQLt.RunAll"
+Write-host Install tSQLt
+new-item -name target/tSQLt -ItemType directory -Force
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -Q "ALTER DATABASE master SET TRUSTWORTHY ON;"
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -i "tSQLt/tSQLt.class.sql" -o "target/tSQLt/Sales_Database_tSQLt_Install.txt"
+
+Write-host Create database
+liquibase --changeLogFile=src/test/db/changelog-with-unit-tests.xml update
+
+Write-host Run the tSQLt unit tests
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -Q "sp_configure @configname=clr_enabled, @configvalue=1"
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -Q "RECONFIGURE"
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -Q "EXEC tSQLt.RunAll"
+
+Write-host Make sure that the unit tests ran
+sqlcmd -S localhost,1433 -U SA -P 'Strong!Passw0rd' -Q "EXEC tSQLt.RunAll"
--- readme.md
+++ readme.md
@@ -9,9 +9,6 @@ Things I've learned:
 A sample docker command to start the Windows Container for MSSQL Server from PowerShell.  It is over 15GB in size.  Makes you wonder what the hell Microsoft has stuffed inside it!!
 docker run -d -p 1433:1433 -e sa_password=Strong!Password -e ACCEPT_EULA=Y microsoft/mssql-server-windows-developer:latest  
 
-TODOs:
-* docker_start_sqlsvrtest_container.sh to build and test a tiny userid/password table
-* docker_start_sqlsvrprod_container.sh
-* Cucumber for Java testing of passwords.feature through wiremock
-* SpecFlow testing of passwords.feature on PasswordRules and PasswordStrength using same passwords.feature 
-* xUnit tests for PasswordRules and PasswordStrength
+Had to install the entire SQL Server 2017 Development Suite, and then the entire set of SQL Server Management Tools simply to get a workable sqlcmd to work from the windows command line (PowerShell).  Micrsoft.  Really?
+
+Using tSQLt_V1.0.5873.27393 from https://tsqlt.org/
