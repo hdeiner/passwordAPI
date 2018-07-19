044313a81e8fc82ac0a6f79c5ca3fcd9d6b19422
howarddeiner <howard.deiner@deinersoft.com>
2018-07-19 13:18:46 -0400

    Remove some unused imports in main.

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,9 @@
 package main;
 
-import org.glassfish.grizzly.Context;
 import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.server.naming.NamingException;
-import org.glassfish.hk2.api.ServiceLocator;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 
-import javax.naming.InitialContext;
 import java.io.IOException;
 import java.net.URI;
 
